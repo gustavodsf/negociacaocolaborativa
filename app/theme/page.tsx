d39f6d55d0badb0395b246bd7e9c13c0c2267f8b
gdanis <gustavodsf1@gmail.com>
2024-05-18 20:02:21 -0300

    add login page

--- app/theme/page.tsx
+++ app/theme/page.tsx
@@ -50,7 +50,7 @@ export default async function Login() {
             />
             <Link 
               className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 '
-              href="/#"
+              href="/signin"
             >
               <SendGAEvent
                   action='clicked_to_login'
