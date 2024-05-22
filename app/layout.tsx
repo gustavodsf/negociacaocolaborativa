0f64a08627bf5645d1a9f36d0df6450195c86bfc
gdanis <gustavodsf1@gmail.com>
2024-05-22 05:48:02 -0300

    fix perspective screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
     >
       <GoogleAnalytics />
       <body className="bg-background text-foreground">
-        <main className="relative flex min-h-screen flex-col items-center">
+        <main className="relative flex min-h-screen flex-col items-center p-2">
           <NextUIProvider>
             <Providers>
               {children}
